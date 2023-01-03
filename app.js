ce83d8789311687eff0be8bf6d45e99700e9e855
ripalkumar <patelripalacc@gmail.com>
2023-01-03 16:05:35 -0500

    done

--- app.js
+++ app.js
@@ -4,16 +4,70 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
 
+let add = a + b ; // 24
+let minus = a - b ; // 16
+let multiply = a * b ; // 80
+let dividing = a / b ; // 5
+console.log("EXERCISE 1:\n==========\n");
+console.log(`let add = a + b ; //24`);
+console.log(`let minus = a - b ; //16`);
+console.log(`let multiply = a * b ; //80`);
+console.log(`let dividing = a / b ; // 5`);
 // YOUR CODE HERE
 
 // Exercise 2
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+num + str ; // '1111'
+num + str2 ; // '11eleven'
+num + isPresent ; // 12
+firstName + num ; // 'Frodo11'
+isPresent + str ; // 'true11'
+firstName + lastName ; // 'FrodoBaggins'
 console.log("EXERCISE 2:\n==========\n");
+console.log(`num + str ; // '1111'`);
+console.log(`num + str2 ; // '11eleven'`);
+console.log(`num + isPresent ; // 12`);
+console.log(`firstName + num ; // 'Frodo11'`);
+console.log(`isPresent + str ; // 'true11'`);
+console.log(`firstName + lastName ; // 'FrodoBaggins'`);
 
 // YOUR CODE HERE
 
 // Exercise 3
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+val == str3 ; // true
+val === str3 ; // false
+!isPresent ; // true
+("eleven" == str4 && val >= str3) ; // false
+(!isPresent2 || isPresent2) ; // true
+0 == false ; // true
+0 === false ; // false
+0 != false ; // false
+0 !== false ; // true
+
+
 console.log("EXERCISE 3:\n==========\n");
+console.log(`val == str3 ; // true`);
+console.log(`val === str3 ; // false`);
+console.log(`!isPresent ; // true`);
+console.log(`("eleven" == str4 && val >= str3) ; // false`);
+console.log(`(!isPresent2 || isPresent2) ; // true`);
+console.log(`0 == false ; // true`);
+console.log(`0 === false ; // false`);
+console.log(`0 != false ; // false`);
+console.log(`0 !== false ; // true`);
 
 // YOUR CODE HERE
